6bed3137404ad8efbb1ab1feb0baea685e1e9dfd
Zakaria <zakjama0@gmail.com>
2024-04-02 10:46:35 +0100

    fetch added

---
+++ src/container/ChatContainer.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const ChatContainer = () => {
+//    const [users, setUsers] = useState([]);
+//    const  [chats, setChats] = useState([]);
+   const [chatroom, setChatroom] = useState([]);
+
+   const fetchChatroom = async () =>{
+    const response = await fetch('http://localhost:8080/chatrooms')
+    const data = await response.json()
+    setChatroom(data)
+   }
+
+   useEffect(() =>{
+    fetchChatroom()
+    console.log(chatroom)
+   }, [])
+
+//    const chatroomTest = chatroom.map( chatroom =>{
+//     return <p>{chatroom.name}</p>
+//    })
+
+   return ( <>
+   {/* {chatroomTest}
+     */}
+    
+    </> );
+}
+ 
+export default ChatContainer;
