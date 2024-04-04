5d3298ef775dd0fd940a9691a06cf1031967c215
BirindarK <birindar.bahia01@gmail.com>
2024-04-04 15:22:38 +0100

    ChatList CSS

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -3,10 +3,11 @@ import {  useNavigate, Outlet } from "react-router-dom";
 const ChatList = ({ chatrooms }) => {
     const navigate = useNavigate();
     const mapChatList = chatrooms.map((chatroom) => {
-        return 
-        <div className = "image">
-        <button onClick={ () => navigate(`${chatroom.id}`)} >{chatroom.name}</button>  
-        </div>      
+        return (
+        <div className="image">
+            <img src="https://media.gq.com/photos/5ba143d4fc6c6260e811638b/master/pass/Toad-Alternatives-GQ-2018-091818.jpg" alt="chatroom image" /> 
+            <button onClick={ () => navigate(`${chatroom.id}`)} >{chatroom.name}</button>
+        </div> ) 
     })
      
     return (
