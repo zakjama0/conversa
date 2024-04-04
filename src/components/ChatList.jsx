1df8300f2b4cff01188cdf7c3f8662239d2e6af5
Saba Kia <skia6589@gmail.com>
2024-04-04 15:59:33 +0100

    more css

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -5,7 +5,6 @@ const ChatList = ({ chatrooms }) => {
     const mapChatList = chatrooms.map((chatroom) => {
         return (
         <div className="image">
-            <img src="https://media.gq.com/photos/5ba143d4fc6c6260e811638b/master/pass/Toad-Alternatives-GQ-2018-091818.jpg" alt="chatroom image" /> 
             <button onClick={ () => navigate(`${chatroom.id}`)} >{chatroom.name}</button>
         </div> ) 
     })
